65e8c50ba2dd8c725ef4a1755f0c411e60e28029
Mickey <lamlexuan99@gmail.com>
2022-03-28 14:00:10 +1100

    edit for new UR3, comment out old path

--- A1_Main.m
+++ A1_Main.m
@@ -2,12 +2,12 @@ clear all; clf; clc;
 
 %% Initialise UR3 model and its base position 
 myUR3 = UR3();
-myUR3.model.base = transl(0.4, 0, 0);
+myUR3.model.base = transl(0.6, -0.3, -0.025) * rpy2tr(pi/2,0,0);
 myUR3.PlotAndColourRobot();
 
 %% Initialise UR5 Linear model and its base position
 linearUR5 = LinearUR5();
-linearUR5.model.base = transl(-0.75, 0.35, -0.025) * trotx(pi/2) * troty(pi);
+linearUR5.model.base = transl(-0.75, 0.35, -0.025) * rpy2tr(pi/2,pi,0);
 linearUR5.PlotAndColourRobot();
 
 hold on;
@@ -42,7 +42,7 @@ steps = 100;
 % Initial q and Ready q
 % UR3:
 qInitUR3 = myUR3.model.getpos();
-qHomeUR3 = deg2rad([0 22 90 -22 -90 180]);
+qHomeUR3 = deg2rad([0 0 -22 -90 22 90 0]);
 
 % UR5:
 qInitUR5 = linearUR5.model.getpos();
@@ -67,50 +67,57 @@ qCurrUR5 = qHomeUR5;
 % %% Place Brick 9 onto Brick 6 and update current q
 % qCurrUR3 = PickAndPlace.UR3PlaceBrickOnBrick(myUR3,qCurrUR3,Brick9,Brick6,steps);
 
-%% Build Wall Sequence
+% %% Build Wall Sequence
+% % UR3 pick Brick 2, UR5 pick Brick 4
+% [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick2,linearUR5,qCurrUR5,Brick4,steps);
+% 
+% % UR3 place Brick 2 on Brick 1, UR5 place Brick 4 next to Brick 1
+% [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick2,Brick1,PickAndPlace.Top ...
+%     ,linearUR5,qCurrUR5,Brick4,Brick1,PickAndPlace.Left,steps);
+% 
+% % UR3 pick Brick 9, UR5 pick Brick 2
+% [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick9,linearUR5,qCurrUR5,Brick2,steps);
+% 
+% % UR3 place Brick 9 on Brick 1, UR5 place Brick 2 next to Brick 4
+% [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick9,Brick1,PickAndPlace.Top ...
+%     ,linearUR5,qCurrUR5,Brick2,Brick4,PickAndPlace.Top,steps);
+% 
+% 
+% 
+% % UR3 pick Brick 6, UR5 pick Brick 5
+% [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick6,linearUR5,qCurrUR5,Brick5,steps);
+% 
+% % UR3 place Brick 6 on Brick 9, UR5 place Brick 5 next to Brick 1
+% [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick6,Brick9,PickAndPlace.Top ...
+%     ,linearUR5,qCurrUR5,Brick5,Brick1,PickAndPlace.Right,steps);
+% 
+% % Move UR3 to Init, Move UR5 to Home
+% MoveIt.MoveTwoRobot(myUR3,qCurrUR3,qInitUR3,linearUR5,qCurrUR5,qHomeUR5,steps);
+% qCurrUR5 = qHomeUR5;
+% 
+% % UR5 pick Brick 8
+% qCurrUR5 = PickAndPlace.UR5PickBrick(linearUR5,qCurrUR5,Brick8,steps);
+% 
+% % UR5 place Brick 8 on Brick 5
+% qCurrUR5 = PickAndPlace.UR5PlaceBrickOnBrick(linearUR5,qCurrUR5,Brick8,Brick5,steps);
+% 
+% % UR5 pick Brick 3
+% qCurrUR5 = PickAndPlace.UR5PickBrick(linearUR5,qCurrUR5,Brick3,steps);
+% 
+% % UR5 place Brick 3 on Brick 8
+% qCurrUR5 = PickAndPlace.UR5PlaceBrickOnBrick(linearUR5,qCurrUR5,Brick3,Brick8,steps);
+% 
+% % UR5 pick Brick 7
+% qCurrUR5 = PickAndPlace.UR5PickBrick(linearUR5,qCurrUR5,Brick7,steps);
+% 
+% % UR5 place Brick 7 on Brick 2
+% qCurrUR5 = PickAndPlace.UR5PlaceBrickOnBrick(linearUR5,qCurrUR5,Brick7,Brick2,steps);
+% MoveIt.MoveOneRobot(linearUR5,qCurrUR5,qInitUR5,steps);
 
-% UR3 pick Brick 2, UR5 pick Brick 4
-[qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick2,linearUR5,qCurrUR5,Brick4,steps);
-
-% UR3 place Brick 2 on Brick 1, UR5 place Brick 4 next to Brick 1
-[qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick2,Brick1,PickAndPlace.Top ...
-    ,linearUR5,qCurrUR5,Brick4,Brick1,PickAndPlace.Left,steps);
-
-% UR3 pick Brick 9, UR5 pick Brick 2
-[qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick9,linearUR5,qCurrUR5,Brick2,steps);
-
-% UR3 place Brick 9 on Brick 1, UR5 place Brick 2 next to Brick 4
-[qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick9,Brick1,PickAndPlace.Top ...
-    ,linearUR5,qCurrUR5,Brick2,Brick4,PickAndPlace.Top,steps);
+%% Build Wall Sequence 2
 
 
 
-% UR3 pick Brick 6, UR5 pick Brick 5
-[qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick6,linearUR5,qCurrUR5,Brick5,steps);
 
-% UR3 place Brick 6 on Brick 9, UR5 place Brick 5 next to Brick 1
-[qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick6,Brick9,PickAndPlace.Top ...
-    ,linearUR5,qCurrUR5,Brick5,Brick1,PickAndPlace.Right,steps);
 
-% Move UR3 to Init, Move UR5 to Home
-MoveIt.MoveTwoRobot(myUR3,qCurrUR3,qInitUR3,linearUR5,qCurrUR5,qHomeUR5,steps);
-qCurrUR5 = qHomeUR5;
 
-% UR5 pick Brick 8
-qCurrUR5 = PickAndPlace.UR5PickBrick(linearUR5,qCurrUR5,Brick8,steps);
-
-% UR5 place Brick 8 on Brick 5
-qCurrUR5 = PickAndPlace.UR5PlaceBrickOnBrick(linearUR5,qCurrUR5,Brick8,Brick5,steps);
-
-% UR5 pick Brick 3
-qCurrUR5 = PickAndPlace.UR5PickBrick(linearUR5,qCurrUR5,Brick3,steps);
-
-% UR5 place Brick 3 on Brick 8
-qCurrUR5 = PickAndPlace.UR5PlaceBrickOnBrick(linearUR5,qCurrUR5,Brick3,Brick8,steps);
-
-% UR5 pick Brick 7
-qCurrUR5 = PickAndPlace.UR5PickBrick(linearUR5,qCurrUR5,Brick7,steps);
-
-% UR5 place Brick 7 on Brick 2
-qCurrUR5 = PickAndPlace.UR5PlaceBrickOnBrick(linearUR5,qCurrUR5,Brick7,Brick2,steps);
-MoveIt.MoveOneRobot(linearUR5,qCurrUR5,qInitUR5,steps);
