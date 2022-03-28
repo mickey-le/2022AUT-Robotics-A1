50bc886782019beccc891b7f015844ebbf31d358
Mickey <lamlexuan99@gmail.com>
2022-03-28 13:59:43 +1100

    test new UR3

--- test.m
+++ test.m
@@ -1,2 +1,5 @@
-testBrick = Brick(transl(0,0,0) * rpy2tr(0,0,0));
-
+% testBrick = Brick(transl(0,0,0) * rpy2tr(0,0,0));
+clear all; clf; clc;
+myUR3 = UR3();
+myUR3.model.base = transl(0.4, 0, 0) * rpy2tr(pi/2,0,0);
+myUR3.PlotAndColourRobot();
