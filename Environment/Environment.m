cb4189698943d80cbcaff83faed388f8e73bcea2
Mickey <lamlexuan99@gmail.com>
2022-03-29 15:19:55 +1100

    add Environment

---
+++ Environment/Environment.m
@@ -0,0 +1,39 @@
+classdef Environment < handle
+    % Private variables for Environment class
+    properties (Access = private)
+        pose;
+        poseUpdate;
+        vertexCount;
+        enviMesh;
+        enviVertices;
+    end
+    % Public functions for usage
+    methods
+        % Class constructor
+        function self = Environment()
+            enviPose = transl(0,0,0) * rpy2tr(0,0,0);
+            self.SetEnviPose(enviPose);
+        end
+        % Setter for environment pose
+        function SetEnviPose(self,enviPose)
+            self.pose = enviPose;
+        end
+        % Getter for environment pose
+        function enviPose = GetEnviPose(self)
+            enviPose = self.pose;
+        end
+        % Function for plotting the environment
+        function PlotEnvironment(self)
+            [face,vertices,data] = plyread('FullEnvi.ply','tri');
+            self.vertexCount = size(vertices,1);
+            midPoint = sum(vertices)/self.vertexCount;
+            self.enviVertices = vertices - repmat(midPoint,self.vertexCount,1);
+            vertexColours = [data.vertex.red, data.vertex.green, data.vertex.blue] / 255;
+            self.enviMesh = trisurf(face, self.enviVertices(:,1), self.enviVertices(:,2), self.enviVertices(:,3) ...
+                , 'FaceVertexCData', vertexColours, 'EdgeColor', 'interp', 'EdgeLighting','flat');
+            self.poseUpdate = [self.pose * [self.enviVertices,ones(self.vertexCount,1)]']';
+            self.enviMesh.Vertices = self.poseUpdate(:,1:3);
+            drawnow();
+        end
+    end
+end
