50f2c90c6a92fa4c1da8adaae35e7d635250a452
Mickey <lamlexuan99@gmail.com>
2022-04-18 11:47:21 +1000

    fix path for LinearUR5

--- A1_Main.m
+++ A1_Main.m
@@ -1,7 +1,7 @@
 %% Initialise
 clear all; clf; clc;
 addpath 'Brick'
-addpath 'LinearUR3'
+addpath 'LinearUR5'
 addpath 'MyUR3'
 addpath 'Environment'
 hold on;
@@ -94,100 +94,100 @@ qCurrUR5 = qHomeUR5;
 % qCurrUR3 = PickAndPlace.UR3PlaceBrickOnBrick(myUR3,qCurrUR3,Brick1,Checkpoint,steps);
 
 %% Build Wall Sequence
-% UR3 pick Brick 1, UR5 pick Brick 2
+%% UR3 pick Brick 1, UR5 pick Brick 2
 disp('UR3 pick 1, UR5 pick 2');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick1,linearUR5,qCurrUR5,Brick2,steps);
 
-% UR3 place Brick 1 on Goal, UR5 place Brick 2 on Checkpoint
+%% UR3 place Brick 1 on Goal, UR5 place Brick 2 on Checkpoint
 disp('UR3 place 1 at goal, UR5 place 2 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick1,Goal,PickAndPlace.Top ...
     ,linearUR5,qCurrUR5,Brick2,Checkpoint,PickAndPlace.Top,steps);
 
-% UR3 pick Brick 2, UR5 pick Brick 3
+%% UR3 pick Brick 2, UR5 pick Brick 3
 disp('UR3 pick 2, UR5 pick 3');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick2,linearUR5,qCurrUR5,Brick3,steps);
 
-% UR3 place Brick 2 on right side of Brick 1, UR5 place Brick 3 on Checkpoint
+%% UR3 place Brick 2 on right side of Brick 1, UR5 place Brick 3 on Checkpoint
 disp('UR3 place 2 right side 1, UR5 place 3 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick2,Brick1,PickAndPlace.Right ...
     ,linearUR5,qCurrUR5,Brick3,Checkpoint,PickAndPlace.Top,steps);
 
-% UR3 pick Brick 3, UR5 pick Brick 4
+%% UR3 pick Brick 3, UR5 pick Brick 4
 disp('UR3 pick 3, UR5 pick 4');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick3,linearUR5,qCurrUR5,Brick4,steps);
 
-% UR3 place Brick 3 on left side of Brick 1, UR5 place Brick 4 on Checkpoint
+%% UR3 place Brick 3 on left side of Brick 1, UR5 place Brick 4 on Checkpoint
 disp('UR3 place 3 left side 1, UR5 place 4 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick3,Brick1,PickAndPlace.Left ...
     ,linearUR5,qCurrUR5,Brick4,Checkpoint,PickAndPlace.Top,steps);
 
-% UR3 pick Brick 4, UR5 pick Brick 5
+%% UR3 pick Brick 4, UR5 pick Brick 5
 disp('UR3 pick 4, UR5 pick 5');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick4,linearUR5,qCurrUR5,Brick5,steps);
 
-% UR3 place Brick 4 on top of Brick 3, UR5 place Brick 5 on Checkpoint
+%% UR3 place Brick 4 on top of Brick 3, UR5 place Brick 5 on Checkpoint
 disp('UR3 place 4 on 3, UR5 place 5 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick4,Brick3,PickAndPlace.Top ...
     ,linearUR5,qCurrUR5,Brick5,Checkpoint,PickAndPlace.Top,steps);
 
-% Adjust UR3, Adjust UR5
+%% Adjust UR3, Adjust UR5
 qMidwayUR3 = qHomeUR3;
 qMidwayUR5 = qHomeUR5;
 MoveIt.MoveTwoRobot(myUR3,qCurrUR3,qMidwayUR3,linearUR5,qCurrUR5,qMidwayUR5,steps);
 qCurrUR3 = qMidwayUR3;
 qCurrUR5 = qMidwayUR5;
 
-% UR3 pick Brick 5, UR5 pick Brick 6
+%% UR3 pick Brick 5, UR5 pick Brick 6
 disp('UR3 pick 5, UR5 pick 6');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick5,linearUR5,qCurrUR5,Brick6,steps);
 
-% UR3 place Brick 5 on top of Brick 1, UR5 place Brick 6 on Checkpoint
+%% UR3 place Brick 5 on top of Brick 1, UR5 place Brick 6 on Checkpoint
 disp('UR3 place 5 on 1, UR5 place 6 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick5,Brick1,PickAndPlace.Top ...
     ,linearUR5,qCurrUR5,Brick6,Checkpoint,PickAndPlace.Top,steps);
 
-% UR3 pick Brick 6, UR5 pick Brick 7
+%% UR3 pick Brick 6, UR5 pick Brick 7
 disp('UR3 pick 6, UR5 pick 7');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick6,linearUR5,qCurrUR5,Brick7,steps);
 
-% UR3 place Brick 6 on top of Brick 2, UR5 place Brick 7 on Checkpoint
+%% UR3 place Brick 6 on top of Brick 2, UR5 place Brick 7 on Checkpoint
 disp('UR3 place 6 on 2, UR5 place 7 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick6,Brick2,PickAndPlace.Top ...
     ,linearUR5,qCurrUR5,Brick7,Checkpoint,PickAndPlace.Top,steps);
 
-% UR3 pick Brick 7, UR5 pick Brick 8
+%% UR3 pick Brick 7, UR5 pick Brick 8
 disp('UR3 pick 7, UR5 pick 8');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick7,linearUR5,qCurrUR5,Brick8,steps);
 
-% UR3 place Brick 7 on top of Brick 4, UR5 place Brick 8 on Checkpoint
+%% UR3 place Brick 7 on top of Brick 4, UR5 place Brick 8 on Checkpoint
 disp('UR3 place 7 on 4, UR5 place 8 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick7,Brick4,PickAndPlace.Top ...
     ,linearUR5,qCurrUR5,Brick8,Checkpoint,PickAndPlace.Top,steps);
 
-% UR3 pick Brick 8, UR5 pick Brick 9
+%% UR3 pick Brick 8, UR5 pick Brick 9
 disp('UR3 pick 8, UR5 pick 9');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPickBrick(myUR3,qCurrUR3,Brick8,linearUR5,qCurrUR5,Brick9,steps);
 
-% UR3 place Brick 8 on top of Brick 5, UR5 place Brick 9 on Checkpoint
+%% UR3 place Brick 8 on top of Brick 5, UR5 place Brick 9 on Checkpoint
 disp('UR3 place 8 on 5, UR5 place 9 at checkpoint');
 [qCurrUR3,qCurrUR5] = PickAndPlace.TwoRobotPlaceBrick(myUR3,qCurrUR3,Brick8,Brick5,PickAndPlace.Top ...
     ,linearUR5,qCurrUR5,Brick9,Checkpoint,PickAndPlace.Top,steps);
 
-% Adjust UR3, UR5 return init state
+%% Adjust UR3, UR5 return init state
 disp('UR5 return initial state');
 qMidwayUR3 = qHomeUR3;
 MoveIt.MoveTwoRobot(myUR3,qCurrUR3,qMidwayUR3,linearUR5,qCurrUR5,qInitUR5,steps);
 qCurrUR3 = qMidwayUR3;
 
-% UR3 pick Brick 9
+%% UR3 pick Brick 9
 disp('UR3 pick 9');
 qCurrUR3 = PickAndPlace.UR3PickBrick(myUR3,qCurrUR3,Brick9,steps);
 
-% UR3 place Brick 9 on Brick 6
+%% UR3 place Brick 9 on Brick 6
 disp('UR3 place 9 on 6');
 qCurrUR3 = PickAndPlace.UR3PlaceBrickOnBrick(myUR3,qCurrUR3,Brick9,Brick6,steps);
 
-% UR3 
+%% UR3 
 disp('UR3 return initial state');
 MoveIt.MoveOneRobot(myUR3,qCurrUR3,qInitUR3,steps);
 
